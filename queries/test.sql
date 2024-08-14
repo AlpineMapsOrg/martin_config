67bcf9feab360921a340c406daa7cac99059ba83
Lucas Dworschak <e1225883@student.tuwien.ac.at>
2024-08-14 10:14:01 +0200

    - changed importance from decimal to real (so that importance is transmitted as a floating point number)

--- queries/test.sql
+++ queries/test.sql
@@ -1,10 +1,11 @@
 
 
 
+DROP FUNCTION IF EXISTS poi_v1;
 
 
 -- SELECT * from metahelper_webcams(10,10) ORDER BY entries_with_values DESC;
 
 
 -- SELECT distinct skeys(data) from webcams_temp;
-SELECT distinct data->'surveillance:zone' from webcams_temp;
+-- SELECT distinct data->'surveillance:zone' from webcams_temp;
