1400f561ce0a652503dcc01d3f4b7275a08e1b6c
Adam Ce <5292991+adam-ce@users.noreply.github.com>
2025-07-06 22:32:49 +0200

    fix update_docker for any system

--- update_docker.sh
+++ update_docker.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 docker pull ghcr.io/maplibre/martin
 docker container rm -f martin
 docker run --name martin \
 	--net=host \
-	--restart=always --detach -p 3000:3000 -v /media/Data/Studium/Master/Projects/AlpineMapsOrg/martin/config/:/config \
+	--restart=always --detach -p 3000:3000 -v ${SCRIPT_DIR}/config/:/config \
 	ghcr.io/maplibre/martin --config config/config.yaml
